3f7cd6ab3016f2ff4a64f446fdcc6e35b3b6edf5
therealsaumil <saumil@saumil.net>
2020-09-11 19:10:10 +0530

    September 2020

--- armxvm.sh
+++ armxvm.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
-rsync -av README.TXT \
-          LICENSE.TXT \
-          dtb \
-          hostfs \
-          run \
-          template \
-          devices \
-          qemuopts \
-          root@10.0.1.250:/armx/
-
+rsync -vrlptD README.TXT \
+              LICENSE.TXT \
+              dtb \
+              hostfs \
+              run \
+              template \
+              devices \
+              qemuopts \
+              armx@10.0.1.250:/armx/
