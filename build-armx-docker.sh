9a6aee7b3e0db87214c968733fbea841fcfbaedc
therealsaumil <saumil@saumil.net>
2021-06-01 17:31:39 +0530

    Dockerized version

--- build-armx-docker.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-OWNERNAME="therealsaumil"
-IMAGENAME="armx"
-TAGNAME="04-2021"
-
-docker build -t $OWNERNAME/$IMAGENAME:$TAGNAME -f Dockerfile .
-
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -16,6 +16,19 @@ sudo exportfs
 #sudo rpc.mountd --debug all --no-nfs-version 2 --no-nfs-version 3 --nfs-version 4
 sudo rpc.mountd --debug all
 
+#echo [+] Starting tinyproxy
+#sudo tinyproxy
+echo "[+] Setting up forwarded ports ${PORTFWD}"
+
+IFS=',' read -ra PORTLIST <<< "${PORTFWD}"
+for PORTPAIR in "${PORTLIST[@]}"
+do
+   SPORT=$(echo ${PORTPAIR} | cut -d':' -f1)
+   DPORT=$(echo ${PORTPAIR} | cut -d':' -f2)
+   echo "[+] mapping port ${SPORT} -> 192.168.100.2:${DPORT}"
+   socat TCP-LISTEN:${SPORT},fork,reuseaddr TCP:192.168.100.2:${DPORT} &
+done
+
 echo '    _ ___ __  ___  __'
 echo '   / \ _ \  \/ \ \/ /  by Saumil Shah | The ARM Exploit Laboratory'
 echo '  / _ \  / |\/) )  (   @therealsaumil | armx.exploitlab.net'
